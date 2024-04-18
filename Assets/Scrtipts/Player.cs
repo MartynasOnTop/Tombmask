a2935336c4bd3ec9207587d45617ad264ad6873b
MartynasOnTop <ponasniekas10@gmail.com>
2024-04-18 20:12:55 +0300

    more maps better movement

--- Assets/Scrtipts/Player.cs
+++ Assets/Scrtipts/Player.cs
@@ -12,6 +12,10 @@ public class Player : MonoBehaviour
     AudioSource source;
     public AudioClip jump;
     public AudioClip land;
+    public AudioClip coinSound;
+    public AudioClip bigCoinsSound;
+    public AudioClip starSound;
+    bool hasLanded;
 
     private void Start()
     {
@@ -22,28 +26,43 @@ public class Player : MonoBehaviour
     async void Update()
     {
         var newinput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        if(Mathf.Abs(newinput.x) > 0 && Mathf.Abs(newinput.y) > 0)
+        {
+            newinput.y = 0;
+        }
+
+        if (rb.velocity.magnitude < 0.1f && !hasLanded && newinput != input)
+        {
+            Instantiate(landingParticles, transform.position, Quaternion.identity);
+            source.PlayOneShot(land);
+            hasLanded = true;
+        }
+
         if( newinput != Vector2.zero && rb.velocity.magnitude < 0.1f)
         {
             input = newinput;
             transform.up = input * -1f;
+            hasLanded = false;
             source.PlayOneShot(jump);
         }
         rb.velocity = input * moveSpeed;
     }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Ground")
-        {
-            Instantiate(landingParticles, transform.position, Quaternion.identity);
-            source.PlayOneShot(land);
-        }
-    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Collectible")
+        if (collision.gameObject.name.Contains("Star"))
         {
             Destroy(collision.gameObject);
-            collision.GetComponent<PlaySound>().PlayS();
+            AudioSystem.Play(starSound);
+        }
+        if (collision.gameObject.name.Contains("Money"))
+        {
+            Destroy(collision.gameObject);
+            AudioSystem.Play(bigCoinsSound);
+        }
+        if (collision.gameObject.name.Contains("Coin"))
+        {
+            Destroy(collision.gameObject);
+            AudioSystem.Play(coinSound);
         }
     }
 }
